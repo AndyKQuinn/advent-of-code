780eb16dd82bbd5af1ec4ae8c073d1f2e29accc2
Andy Quinn <aquinn@Andys-Mac-mini.local>
2025-12-07 11:01:18 -0600

    create template, add day6

---
+++ template/main.py
@@ -0,0 +1,17 @@
+def puzzle_one():
+    print("Processing puzzle 1...")
+
+    with open("inputs.txt", "r") as f:
+        content = f.read().strip()
+
+
+def puzzle_two():
+    print("Processing puzzle 2...")
+
+    with open("inputs.txt", "r") as f:
+        content = f.read().strip()
+
+
+if __name__ == "__main__":
+    puzzle_one()
+    # puzzle_two()
